cc0ac946720589b1b35b44ddf95e2735455f7973
Ricardo <a01337222@itesm.mx>
2021-04-24 20:34:39 -0500

    segunda

--- app/src/main/java/edu/itesm/proyectop/RegisterActivity.kt
+++ app/src/main/java/edu/itesm/proyectop/RegisterActivity.kt
@@ -1,11 +1,69 @@
 package edu.itesm.proyectop
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import edu.itesm.proyectop.databinding.ActivityLoginBinding
+import edu.itesm.proyectop.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var bind: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        bind = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        auth = Firebase.auth
+        setRegister()
+    }
+
+    private fun setRegister() {
+
+        bind.registerbtn.setOnClickListener {
+
+            if (bind.correo.text.isNotEmpty() && bind.password.text.isNotEmpty()) {
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+
+                    bind.correo.text.toString(),
+                    bind.password.text.toString()
+
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        usuarioCreado()
+
+                    }
+                }.addOnFailureListener {
+                    Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+
+                }
+
+
+            }
+        }
+    }
+    val positiveButtonClick = { dialog: DialogInterface, which: Int ->
+        finish()
+    }
+
+    private fun usuarioCreado() {
+        val builder = AlertDialog.Builder(this)
+        with(builder) {
+            setTitle("successful")
+            setMessage("Usuario creado con éxito!")
+            setPositiveButton("ok", DialogInterface.OnClickListener(function = positiveButtonClick))
+            show()
+        }
+
     }
 }
